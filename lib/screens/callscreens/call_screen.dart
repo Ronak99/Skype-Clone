4d20d72a1ffcf02d74a91486fe862d4dce75e8d9
Ronak99 <punase.ronak99@gmail.com>
2020-03-18 10:05:04 +0530

    upto part 9

--- lib/screens/callscreens/call_screen.dart
+++ lib/screens/callscreens/call_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:provider/provider.dart';
 import 'package:skype_clone/models/call.dart';
+import 'package:skype_clone/provider/user_provider.dart';
 import 'package:skype_clone/resources/call_methods.dart';
 
 class CallScreen extends StatefulWidget {
@@ -14,8 +20,43 @@ class CallScreen extends StatefulWidget {
 }
 
 class _CallScreenState extends State<CallScreen> {
-
   final CallMethods callMethods = CallMethods();
+
+  UserProvider userProvider;
+  StreamSubscription callStreamSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    addPostFrameCallback();
+  }
+
+  addPostFrameCallback() {
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      userProvider = Provider.of<UserProvider>(context, listen: false);
+
+      callStreamSubscription = callMethods
+          .callStream(uid: userProvider.getUser.uid)
+          .listen((DocumentSnapshot ds) {
+        // defining the logic
+        switch (ds.data) {
+          case null:
+            // snapshot is null which means that call is hanged and documents are deleted
+            Navigator.pop(context);
+            break;
+
+          default:
+            break;
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    callStreamSubscription.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
